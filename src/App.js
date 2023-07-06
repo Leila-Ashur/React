69d6e24490112e897f43bd1c8eb0788dc6835c14
leilaashur <leilahashur@gmail.com>
2023-07-06 07:37:26 +0300

    react commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Introduction from './Introduction';
+// import Context from './Introduction/Detail';
+import Detail from './Detail';
+
+
+function App() {
+ 
+  return (
+    <div>
+      <Introduction/>
+      <Detail/>
+  
+          </div>
+  );
+}
+
+export default App;
---
+++ src/Detail/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import'./style.css'
+const Detail= ()=>{
+    return(
+        <div>
+            <h1>
+                What I learnt in React
+            </h1>
+            <p>It is a JS library developed by Facebook</p>
+            <p>It has a reusable components</p>
+            <p>I am software developer</p>
+            <p>I am learning the following</p>
+           < ul>
+           <li>Javascript</li>
+           <li>Django frame work</li>
+           <li>Kotlin</li>
+           <li>Python</li>
+           </ul>
+
+        </div>
+    )
+
+}
+export default Detail;
